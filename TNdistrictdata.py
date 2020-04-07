7057b8f1b435f09ffb289ab88696e6dd7f041062
Ramya <48737377+rumya0@users.noreply.github.com>
2020-04-07 19:32:38 -0400

    Update TNdistrictdata.py

--- TNdistrictdata.py
+++ TNdistrictdata.py
@@ -11,8 +11,6 @@ from datetime import date
 import PyPDF2, io, requests
 import pandas as pd
 
-from bs4 import BeautifulSoup
-
 today = date.today()
 tdate = today.strftime("%d-%m")
 url=("https://stopcorona.tn.gov.in/wp-content/uploads/2020/03/Media-Bulletin-"+tdate+"-20-COVID-19-6-PM.pdf")
@@ -32,15 +30,3 @@ new_df = new_df[:-1]
 new_df.to_json('tndistrict.json', orient = 'index')
 new_df.to_json('tndistrict1.json', orient = 'records')
 
-
-
-url2 = 'https://www.mohfw.gov.in/'
-response = requests.get(url2)
-soup = BeautifulSoup(response.content, 'html5lib')
-table = soup.find_all('table', attrs={'class':'data-table table-responsive'})
-print(table)
-
-#df = pd.DataFrame(newdf1)
-
-# df = newdf1.replace('NaN', 0)
-# newdf1.fillna(0)
